62d1b1b73e957cb548e2ebec598be760285ab3ec
Daniel Bünzli <daniel.buenzli@erratique.ch>
2012-05-02 16:09:25 +0200

    `utftrip`, support for input from a file.

--- test/utftrip.ml
+++ test/utftrip.ml
@@ -8,11 +8,11 @@ let pp = Format.fprintf
 let pp_pos ppf d = pp ppf "%d.%d:(%d) " 
   (Uutf.decoder_line d) (Uutf.decoder_col d) (Uutf.decoder_count d)
 
-let pp_decode ppf d v = pp ppf "%a%a@\n" pp_pos d Uutf.pp_decode v
+let pp_decode inf d ppf v = pp ppf "%s:%a%a@\n" inf pp_pos d Uutf.pp_decode v
 
 let exec = Filename.basename Sys.executable_name
 let log f = Format.eprintf ("%s: " ^^ f ^^ "@?") exec 
-let log_malformed v = log "skipped %a@\n" Uutf.pp_decode v
+let log_malformed inf d v = log "%a" (pp_decode inf d) v
     
 (* IO tools  *)
 
@@ -49,8 +49,22 @@ let string_to_channel use_unix oc s =
   else output_string oc s
 
 let dst_for sout = if sout then `Buffer (Buffer.create 512) else `Channel stdout
-let src_for use_unix sin =
-  if sin then `String (string_of_channel use_unix stdin) else `Channel stdin
+let src_for inf sin use_unix =
+  try 
+    let ic = if inf = "-" then stdin else open_in inf in 
+    if sin then `String (string_of_channel use_unix ic) else `Channel ic
+  with Sys_error e -> log "%s\n" e; exit 1
+
+let close_src src = 
+  try match src with `Channel ic when ic <> stdin -> close_in ic | _ -> () with
+  | Sys_error e -> log "%s\n" e; exit 1
+
+let src_for_unix inf = 
+  try if inf = "-" then Unix.stdin else Unix.(openfile inf [O_RDONLY] 0) with
+  | Unix.Unix_error (e, _, v) -> log "%s: %s\n" (Unix.error_message e) v; exit 1
+
+let close_src_unix fd = try if fd <> Unix.stdin then Unix.close fd with 
+| Unix.Unix_error (e, _, v) -> log "%s: %s\n" (Unix.error_message e) v; exit 1
 
 let rec encode_unix fd s e v = match Uutf.encode e v with `Ok -> () 
 | `Partial ->
@@ -60,56 +74,59 @@ let rec encode_unix fd s e v = match Uutf.encode e v with `Ok -> ()
 
 (* Dump *) 
 
-let dump_ encoding nln src =
-  let rec loop d = match Uutf.decode d with `Await -> assert false
-  | v -> pp_decode Format.std_formatter d v; if v <> `End then loop d
+let dump_ inf encoding nln src =
+  let rec loop inf d = match Uutf.decode d with `Await -> assert false
+  | v -> (pp_decode inf d) Format.std_formatter v; if v <> `End then loop inf d
   in
-  loop (Uutf.decoder ?nln ?encoding src)
+  loop inf (Uutf.decoder ?nln ?encoding src)
     
-let dump_unix encoding nln usize fd =
+let dump_unix inf encoding nln usize fd =
   let rec loop fd s d = match Uutf.decode d with
   | `Await -> 
       let rc = unix_read fd s 0 (String.length s) in 
       Uutf.Manual.src d s 0 rc; loop fd s d
-  | v -> pp_decode Format.std_formatter d v; if v <> `End then loop fd s d
+  | v -> (pp_decode inf d) Format.std_formatter v; if v <> `End then loop fd s d
   in
   loop fd (String.create usize) (Uutf.decoder ?nln ?encoding `Manual) 
 
-let dump sin use_unix usize ie nln = 
-  if sin || not use_unix then dump_ ie nln (src_for use_unix sin) else 
-  dump_unix ie nln usize Unix.stdin
+let dump inf sin use_unix usize ie nln = 
+  if sin || not use_unix then dump_ inf ie nln (src_for inf sin use_unix) else 
+  dump_unix inf ie nln usize (src_for_unix inf)
 
 (* Guess only *)
 
-let guess () = 
-  let d = Uutf.decoder (`Channel stdin) in 
+let guess inf = 
+  let d = Uutf.decoder (src_for inf false false) in 
   ignore (Uutf.decode d); 
   Format.printf "%s@." (Uutf.encoding_to_string (Uutf.decoder_encoding d))
 
 (* Decode only *)
 
-let decode_ encoding nln src =
+let decode_ inf encoding nln src =
+  let malformed = log_malformed inf in 
   let rec loop d = match Uutf.decode d with `Await -> assert false
   | `Uchar _ -> loop d
   | `End -> ()
-  | `Malformed _ as v -> log_malformed v; loop d
+  | `Malformed _ as v -> malformed d v; loop d
   in
-  loop (Uutf.decoder ?nln ?encoding src)
+  loop (Uutf.decoder ?nln ?encoding src); close_src src
 
-let decode_unix encoding nln usize fd =
+let decode_unix inf encoding nln usize fd =
+  let malformed = log_malformed inf in
   let rec loop fd s d = match Uutf.decode d with
   | `Uchar _ -> loop fd s d
   | `End -> () 
-  | `Malformed _ as v -> log_malformed v; loop fd s d
+  | `Malformed _ as v -> malformed d v; loop fd s d
   | `Await -> 
       let rc = unix_read fd s 0 (String.length s) in 
       Uutf.Manual.src d s 0 rc; loop fd s d
   in
-  loop fd (String.create usize) (Uutf.decoder ?nln ?encoding `Manual) 
+  loop fd (String.create usize) (Uutf.decoder ?nln ?encoding `Manual); 
+  close_src_unix fd
 
-let decode sin use_unix usize ie nln =
-  if sin || not use_unix then decode_ ie nln (src_for use_unix sin) else
-  decode_unix ie nln usize Unix.stdin 
+let decode inf sin use_unix usize ie nln =
+  if sin || not use_unix then decode_ inf ie nln (src_for inf sin use_unix) else
+  decode_unix inf ie nln usize (src_for_unix inf)
   
 (* Random encode only *)
 
@@ -147,11 +164,12 @@ let r_encode sout use_unix usize rseed rcount oe =
   
 (* Trip *)
     
-let trip_ nln ie oe src dst = 
+let trip_ inf nln ie oe src dst = 
+  let malformed = log_malformed inf in
   let rec loop d e = function `Await -> assert false
   | `Uchar _ as v -> ignore (Uutf.encode e v); loop d e (Uutf.decode d)
   | `End -> ignore (Uutf.encode e `End)
-  | `Malformed _ as v -> log_malformed v; loop d e (Uutf.decode d)
+  | `Malformed _ as v -> malformed d v; loop d e (Uutf.decode d)
   in
   let d = Uutf.decoder ?nln ?encoding:ie src in 
   let e, first = match oe with 
@@ -165,14 +183,15 @@ let trip_ nln ie oe src dst =
   in
   if (Uutf.encoder_encoding e = `UTF_16 || Uutf.decoder_removed_bom d) 
   then ignore (Uutf.encode e (`Uchar Uutf.u_bom));
-  loop d e first
+  loop d e first; close_src src
 
-let trip_unix usize nln ie oe fdi fdo =
+let trip_unix inf usize nln ie oe fdi fdo =
+  let malformed = log_malformed inf in
   let rec loop fdi fdo ds es d e = function
   | `Uchar _ as v ->
       encode_unix fdo es e v; loop fdi fdo ds es d e (Uutf.decode d)
   | `End -> encode_unix fdo es e `End
-  | `Malformed _ as v -> log_malformed v; loop fdi fdo ds es d e (Uutf.decode d)
+  | `Malformed _ as v -> malformed d v; loop fdi fdo ds es d e (Uutf.decode d)
   | `Await -> 
       let rc = unix_read fdi ds 0 (String.length ds) in 
       Uutf.Manual.src d ds 0 rc; loop fdi fdo ds es d e (Uutf.decode d)
@@ -196,19 +215,19 @@ let trip_unix usize nln ie oe fdi fdo =
   Uutf.Manual.dst e es 0 (String.length es); 
   if (Uutf.encoder_encoding e = `UTF_16 || Uutf.decoder_removed_bom d) 
   then encode_unix fdo es e (`Uchar Uutf.u_bom); 
-  loop fdi fdo ds es d e first
+  loop fdi fdo ds es d e first; close_src_unix fdi
 
-let trip sin sout use_unix usize ie oe nln = 
-  let src = src_for use_unix sin in
+let trip inf sin sout use_unix usize ie oe nln = 
+  let src = src_for inf sin use_unix in
   let dst = dst_for sout in
-  if sin || sout || not use_unix then trip_ nln ie oe src dst else
-  trip_unix usize nln ie oe Unix.stdin Unix.stdout; 
+  if sin || sout || not use_unix then trip_ inf nln ie oe src dst else
+  trip_unix inf usize nln ie oe (src_for_unix inf) Unix.stdout; 
   match dst with `Channel _ -> ()
   | `Buffer b -> string_to_channel use_unix stdout (Buffer.contents b)
 
 let main () = 
   let usage = Printf.sprintf 
-    "Usage: %s <options>\n\
+    "Usage: %s [OPTION]... [INFILE]\n\
     \ recode UTF-{8,16,16LE,16BE} from stdin to stdout.\n\
     \ If no input encoding is specified, it is guessed. If no output\n\
     \ encoding is specified, same as input.\n\
@@ -216,6 +235,11 @@ let main () =
   in
   let cmd = ref `Trip in
   let set_cmd v () = cmd := v in
+  let inf = ref "-" in
+  let set_inf f = 
+    if !inf <> "-" then raise (Arg.Bad "only one file can be specified") else
+    inf := f 
+  in
   let ie = ref None in 
   let ie_fun e = match Uutf.encoding_of_string e with
   | Some e -> ie := Some e 
@@ -260,13 +284,13 @@ let main () =
     "-rcount", Arg.Int (nat "-rcount" rcount), 
     "number of random characters to generate."; ]
   in
-  Arg.parse options (fun _ -> raise (Arg.Bad "illegal argument")) usage;
+  Arg.parse options set_inf usage;
   match !cmd with 
-  | `Dump -> dump !sin !use_unix !usize !ie !nln 
-  | `Guess -> guess ()
-  | `Decode -> decode !sin !use_unix !usize !ie !nln
+  | `Dump -> dump !inf !sin !use_unix !usize !ie !nln 
+  | `Guess -> guess !inf
+  | `Decode -> decode !inf !sin !use_unix !usize !ie !nln
   | `Encode -> r_encode !sout !use_unix !usize !rseed !rcount !oe
-  | `Trip -> trip !sin !sout !use_unix !usize !ie !oe !nln
+  | `Trip -> trip !inf !sin !sout !use_unix !usize !ie !oe !nln
   
 let () = main ()
 
