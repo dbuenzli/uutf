f4b838b4c6266fdf27f7bdcceb54a6b76ef09dfe
Daniel Bünzli <daniel.buenzli@erratique.ch>
2022-09-04 22:57:46 +0200

    Track b0

--- B0.ml
+++ B0.ml
@@ -1,6 +1,4 @@
 open B0_kit.V000
-open B00_std
-
 
 (* OCaml library names *)
 
@@ -27,36 +25,36 @@ let utftrip =
   let doc = "Recode UTF-{8,16,16LE,16BE} and latin1 from stdin to stdout" in
   let srcs = Fpath.[`File (v "test/utftrip.ml")] in
   let requires = [unix; uutf; cmdliner] in
-  B0_ocaml.exe "utftrip" ~doc ~srcs ~requires
+  B0_ocaml.exe "utftrip" ~public:true ~doc ~srcs ~requires
 
 (* Packs *)
 
 let default =
   let meta =
-    let open B0_meta in
-    empty
-    |> tag B0_opam.tag
-    |> add authors ["The uutf programmers"]
-    |> add maintainers ["Daniel Bünzli <daniel.buenzl i@erratique.ch>"]
-    |> add homepage "https://erratique.ch/software/uutf"
-    |> add online_doc "https://erratique.ch/software/uutf/doc/"
-    |> add licenses ["ISC"]
-    |> add repo "git+https://erratique.ch/repos/uutf.git"
-    |> add issues "https://github.com/dbuenzli/uutf/issues"
-    |> add description_tags
+    B0_meta.empty
+    |> B0_meta.(add authors) ["The uutf programmers"]
+    |> B0_meta.(add maintainers)
+       ["Daniel Bünzli <daniel.buenzl i@erratique.ch>"]
+    |> B0_meta.(add homepage) "https://erratique.ch/software/uutf"
+    |> B0_meta.(add online_doc) "https://erratique.ch/software/uutf/doc/"
+    |> B0_meta.(add licenses) ["ISC"]
+    |> B0_meta.(add repo) "git+https://erratique.ch/repos/uutf.git"
+    |> B0_meta.(add issues) "https://github.com/dbuenzli/uutf/issues"
+    |> B0_meta.(add description_tags)
       ["unicode"; "text"; "utf-8"; "utf-16"; "codec"; "org:erratique"]
-    |> add B0_opam.Meta.depopts ["cmdliner", ""]
-    |> add B0_opam.Meta.conflicts
+    |> B0_meta.tag B0_opam.tag
+    |> B0_meta.add B0_opam.depopts ["cmdliner", ""]
+    |> B0_meta.add B0_opam.conflicts
       [ "cmdliner", {|< "1.1.0"|}]
-    |> add B0_opam.Meta.depends
+    |> B0_meta.add B0_opam.depends
       [ "ocaml", {|>= "4.03.0"|};
         "ocamlfind", {|build|};
         "ocamlbuild", {|build|};
         "topkg", {|build & >= "1.0.3"|};
       ]
-    |> add B0_opam.Meta.build
+    |> B0_meta.add B0_opam.build
       {|[["ocaml" "pkg/pkg.ml" "build" "--dev-pkg" "%{dev}%"
           "--with-cmdliner" "%{cmdliner:installed}%"]]|}
   in
-  B0_pack.v "default" ~doc:"uutf package" ~meta ~locked:true @@
+  B0_pack.make "default" ~doc:"uutf package" ~meta ~locked:true @@
   B0_unit.list ()
